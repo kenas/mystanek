e977b5e17fbebeba6e26ecc6fcf1ff6d2dc5e26d
kenas <liborgess@MacBook-Pro.local>
2020-06-30 18:09:32 +0100

    first commit

---
+++ resources/views/navbar/main.blade.php
@@ -0,0 +1,27 @@
+<nav>
+    <div class="navbar">
+        <div class="logo">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46.88 90.72">
+                    <g id="Layer_2" data-name="Layer 2">
+                    <g id="Layer_1-2" data-name="Layer 1">
+                    <rect x="6.1" width="40.78" height="90.72" rx="9.21" transform="translate(52.99 90.72) rotate(180)" style="fill:#908fb2"/>
+                    <rect y="1.47" width="45.13" height="87.83" rx="6.86" transform="translate(45.13 90.76) rotate(180)" style="fill:#fff"/></g>
+                    <g id="Layer_4" data-name="Layer 4"><circle cx="23.81" cy="82" r="4" style="fill:#908fb2"/>
+                    </g>
+                </g>
+            </svg>
+           <h3 class="logo-text">used iPhone</h3>      
+        </div>
+        <ul>
+            <li><a href="">Garance vrácení peněz</a></li>
+            <li><a href="">Podmínky</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Kontakt</a></li>
+        </ul>
+        <div class="hamburger-container">
+            <div class="hamburger"></div>
+            <div class="hamburger"></div> 
+            <div class="hamburger"></div> 
+        </div>
+    </div>
+</nav>
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque debitis quisquam exercitationem accusamus natus quia? Impedit debitis a magni. Minima cumque ex quia minus accusamus ipsa doloremque itaque quisquam ut?
+@endsection
